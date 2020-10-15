c3844f091db6702f7c51e1ae1f9dce2376300c89
Will Lorand <will@milemarker3.org>
2020-10-15 13:28:30 -0700

    impl InfoPanel component

--- src/panels/InfoPanel.tsx
+++ src/panels/InfoPanel.tsx
@@ -13,14 +13,36 @@ function InfoPanel() {
   // local state
   const [fundInfo, setFundInfo] = useState<FundInfo>();
 
-  // fetch data via useEffect (aka componentDidMount)
+  // fetch data by component via useEffect (aka componentDidMount)
+  // better: use a data fetch service file for consolidated data fetching (get confirm)
   useEffect(() => {
     getFundInfo().then((data: FundInfo) => {
-      setFundInfo(data);
+      setFundInfo(data); // pop into local state
     });
   }, []);
 
-  return <h2>My Info Panel</h2>;
+  // return PanelBar, PanelBarItem UI Components and fill with mapped over data
+  return (
+    <PanelBar>
+      <PanelBarItem expanded={true} title="Fund Managers">
+        <div>
+          {fundInfo &&
+            fundInfo.managers.map((manager, i) => (
+              <div className="manager" key={i}>
+                <img
+                  src={`/team/${manager.firstName}${manager.lastName}.png`}
+                  alt={`${manager.firstName}  ${manager.lastName}`}
+                />
+                <span className="manager-info">
+                  <h2>{`${manager.firstName} ${manager.lastName}`}</h2>
+                  <p>{manager.position}</p>
+                </span>
+              </div>
+            ))}
+        </div>
+      </PanelBarItem>
+    </PanelBar>
+  );
 }
 
 export default InfoPanel;
